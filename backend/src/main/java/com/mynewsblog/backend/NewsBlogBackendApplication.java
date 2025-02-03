5acb43587797205ba290a898cde95065ab6dddd0
AzizTuprak <aziz.abdirshid@gmail.com>
2025-02-03 18:46:55 +0100

    project development tools sperated into two by Intellij backend, by VS code frontend and gitignore file updated accordingly, one test java file added for testing if backend working fine

---
+++ backend/src/main/java/com/mynewsblog/backend/NewsBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mynewsblog.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsBlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsBlogBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mynewsblog/backend/NewsBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mynewsblog.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsBlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
